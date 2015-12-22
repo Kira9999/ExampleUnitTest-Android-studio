8e7b619053d4bce71650a81c0a2f1e4c89b1113e
unknown <hachan@kira.rd.sce.pccu.edu.tw>
2015-12-22 18:19:19 +0800

    no message

--- app/src/test/java/com/example/hachan/myapplication/ExampleUnitTest3.java
+++ app/src/test/java/com/example/hachan/myapplication/ExampleUnitTest3.java
@@ -2,16 +2,15 @@ package com.example.hachan.myapplication;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static junit.framework.Assert.assertTrue;
 
 /**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ * Created by hachan on 2015/12/22.
  */
 public class ExampleUnitTest3 {
 
     @Test
-    public void Char1() throws Exception {
-        assertTrue(false);
+    public void char3() throws Exception {
+        assertTrue(true);
     }
-
 }
